ed810e428828f62dc031fe28b163075d6c2ced7d
Shashi960 <shashikodiya@gmail.com>
2026-04-27 17:18:44 +0530

    Add sparkline parameter to fetchCoins API call

--- src/services/api.js
+++ src/services/api.js
@@ -9,7 +9,7 @@ const getOptions = () => {
 
 export const fetchCoins = async () => {
   const response = await fetch(
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1",
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true",
     getOptions()
   );
 
